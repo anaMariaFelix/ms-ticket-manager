092321f0961d907e6d813574119eb31056128973
user.name <anafelix0909@gmail.com>
2025-08-07 18:52:34 -0300

    feat: implementation of the status enum

---
+++ src/main/java/com/anamariafelix/ms_ticket_manager/enums/Status.java
@@ -0,0 +1,25 @@
+package com.anamariafelix.ms_ticket_manager.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
+public enum Status {
+
+    PENDING("pending"),
+    COMPLETED("conpleted"),
+    CANCELED("canceled");
+
+    private final String description;
+
+    Status(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @JsonCreator
+    public static Status fromString(String value) {
+        return Status.valueOf(value.toUpperCase());
+    }
+}
