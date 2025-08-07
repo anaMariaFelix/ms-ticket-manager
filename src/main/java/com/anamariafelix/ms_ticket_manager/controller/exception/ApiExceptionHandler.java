6faaf871e33bf909f3c27c633133beb13c08e6dd
user.name <anafelix0909@gmail.com>
2025-08-07 19:08:46 -0300

    feat: implementação da classe ApiExceptionHandler

---
+++ src/main/java/com/anamariafelix/ms_ticket_manager/controller/exception/ApiExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.anamariafelix.ms_ticket_manager.controller.exception;
+
+import com.anamariafelix.ms_ticket_manager.exception.EventNotFoundException;
+import com.anamariafelix.ms_ticket_manager.exception.OpenFeignConectionException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> methodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request, BindingResult result) {
+        log.error("Api Error - ", e);
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY, "Invalid field(s)", result));
+    }
+
+    @ExceptionHandler(EventNotFoundException.class)
+    public ResponseEntity<ErrorMessage> EventNotFoundException(EventNotFoundException e,HttpServletRequest request) {
+        log.error("Api Error - ", e);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.NOT_FOUND, e.getMessage()));
+    }
+
+    @ExceptionHandler(OpenFeignConectionException.class)
+    public ResponseEntity<ErrorMessage> openFeignConectionException(OpenFeignConectionException e,HttpServletRequest request) {
+        log.error("Api Error - ", e);
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)//503
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.SERVICE_UNAVAILABLE, e.getMessage()));
+    }
+}
