36290a2d00cb2a430a58ac17effa43a4aef3c861
user.name <anafelix0909@gmail.com>
2025-08-07 19:07:44 -0300

    feat: custom exception implementation

---
+++ src/main/java/com/anamariafelix/ms_ticket_manager/exception/EventNotFoundException.java
@@ -0,0 +1,8 @@
+package com.anamariafelix.ms_ticket_manager.exception;
+
+public class EventNotFoundException extends RuntimeException {
+
+    public EventNotFoundException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/main/java/com/anamariafelix/ms_ticket_manager/exception/OpenFeignConectionException.java
@@ -0,0 +1,8 @@
+package com.anamariafelix.ms_ticket_manager.exception;
+
+public class OpenFeignConectionException extends RuntimeException {
+
+    public OpenFeignConectionException(String msg) {
+        super(msg);
+    }
+}
