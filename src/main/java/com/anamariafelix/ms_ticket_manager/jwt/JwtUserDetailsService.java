3e364db749032bf328d5466c854c6ad811fbfb0a
user.name <anafelix0909@gmail.com>
2025-08-08 20:02:10 -0300

    feat: implementation of class JwtUserDetailsService

---
+++ src/main/java/com/anamariafelix/ms_ticket_manager/jwt/JwtUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.anamariafelix.ms_ticket_manager.jwt;
+
+import com.anamariafelix.ms_ticket_manager.model.User;
+import com.anamariafelix.ms_ticket_manager.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    private  final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userService.findByEmail(email);
+
+        return new JwtUserDetails(user);
+    }
+
+    public JwtToken getTokenAuthenticated(String email){
+        User user = userService.findByEmail(email);
+        return JwtUtils.createToken(email, user.getRole().name().substring("Role_".length()));
+    }
+}
