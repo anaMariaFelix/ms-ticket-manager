1eecf6eaf6a70baf6db727574655f3ff3c71e07f
user.name <anafelix0909@gmail.com>
2025-08-08 20:02:47 -0300

    feat: implementation of class SpringSecurityConfig

---
+++ src/main/java/com/anamariafelix/ms_ticket_manager/config/SpringSecurityConfig.java
@@ -0,0 +1,62 @@
+package com.anamariafelix.ms_ticket_manager.config;
+
+import com.anamariafelix.ms_ticket_manager.jwt.JwtAuthorizationFilter;
+import com.anamariafelix.ms_ticket_manager.jwt.jwtAuthenticationEntryPoint;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+@EnableMethodSecurity
+@EnableWebMvc
+@Configuration
+public class SpringSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .formLogin(form -> form.disable())
+                .httpBasic(basic -> basic.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "api/v1/user/create-user").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/v1/ticket/get-ticket-eventId/*").permitAll()
+                        .requestMatchers(HttpMethod.POST, "api/v1/auth").permitAll()
+                        .anyRequest().authenticated()
+                ).sessionManagement(
+                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                ).addFilterBefore(
+                        jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class
+                ).exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(new jwtAuthenticationEntryPoint())
+                ).build();
+
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public JwtAuthorizationFilter jwtAuthorizationFilter() {
+        return new JwtAuthorizationFilter();
+    }
+
+}
