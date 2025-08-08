fc065c18e99281824e00c4f5df49af08fc40acc4
user.name <anafelix0909@gmail.com>
2025-08-08 14:40:55 -0300

    feat: Custom exception implementation TicketUnavailableException

---
+++ src/main/java/com/anamariafelix/ms_ticket_manager/exception/TicketUnavailableException.java
@@ -0,0 +1,8 @@
+package com.anamariafelix.ms_ticket_manager.exception;
+
+public class TicketUnavailableException extends RuntimeException {
+
+    public TicketUnavailableException(String msg) {
+        super(msg);
+    }
+}
