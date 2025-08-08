774c9bc20fe61e9c3ba30031aa56367e5ce4da78
user.name <anafelix0909@gmail.com>
2025-08-08 12:00:26 -0300

    feat: Implementation of the userService class with the create and findByEmail methods

---
+++ src/main/java/com/anamariafelix/ms_ticket_manager/service/UserService.java
@@ -0,0 +1,32 @@
+package com.anamariafelix.ms_ticket_manager.service;
+
+import com.anamariafelix.ms_ticket_manager.exception.UniqueViolationException;
+import com.anamariafelix.ms_ticket_manager.model.User;
+import com.anamariafelix.ms_ticket_manager.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    //private final PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public User create(User user){
+        try{
+            //user.setPassword(passwordEncoder.encode(user.getPassword()));
+            return userRepository.save(user);
+        }catch (DuplicateKeyException e){
+            throw new UniqueViolationException(String.format("Email or CPF already registered!", user.getEmail()));
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public User findByEmail(String username) {
+        return userRepository.findByEmail(username);
+    }
+}
