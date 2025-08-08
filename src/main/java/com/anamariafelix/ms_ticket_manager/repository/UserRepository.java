a01f476a59825680319dbefd9583fc3fac7529e3
user.name <anafelix0909@gmail.com>
2025-08-08 14:35:53 -0300

    feat: Adding custom findByCpf query to UserRepository"

--- src/main/java/com/anamariafelix/ms_ticket_manager/repository/UserRepository.java
+++ src/main/java/com/anamariafelix/ms_ticket_manager/repository/UserRepository.java
@@ -4,8 +4,12 @@ import com.anamariafelix.ms_ticket_manager.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, String>{
 
     User findByEmail(String username);
+
+    Optional<User> findByCpf(String cpf);
 }
