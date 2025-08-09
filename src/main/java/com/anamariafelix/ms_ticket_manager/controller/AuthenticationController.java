b36792ec6f578ed31c586a4c44dbfe05804a9055
user.name <anafelix0909@gmail.com>
2025-08-09 02:27:37 -0300

    feat: authentication endpoint documentation

--- src/main/java/com/anamariafelix/ms_ticket_manager/controller/AuthenticationController.java
+++ src/main/java/com/anamariafelix/ms_ticket_manager/controller/AuthenticationController.java
@@ -2,8 +2,13 @@ package com.anamariafelix.ms_ticket_manager.controller;
 
 import com.anamariafelix.ms_ticket_manager.controller.exception.ErrorMessage;
 import com.anamariafelix.ms_ticket_manager.dto.UserLoginDTO;
+import com.anamariafelix.ms_ticket_manager.dto.UserResponseDTO;
 import com.anamariafelix.ms_ticket_manager.jwt.JwtToken;
 import com.anamariafelix.ms_ticket_manager.jwt.JwtUserDetailsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -28,6 +33,15 @@ public class AuthenticationController {
 
     private final AuthenticationManager authenticationManager;
 
+    @Operation(summary = "Authenticate to the API", description = "Authentication features in the API",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Authentication successful and return of a Bearer token",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = UserResponseDTO.class))),
+                    @ApiResponse(responseCode = "400", description = "Invalid credentials",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "422", description = "Invalid fields",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
+            })
     @PostMapping
     public ResponseEntity<?> authentication(@RequestBody @Valid UserLoginDTO dto, HttpServletRequest request){
 
