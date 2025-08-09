124a2494097d90012b43d4db443fa45b2aab2598
user.name <anafelix0909@gmail.com>
2025-08-09 02:26:16 -0300

    feat: implementation of the SpringDocOpenApiConfig class

---
+++ src/main/java/com/anamariafelix/ms_ticket_manager/config/SpringDocOpenApiConfig.java
@@ -0,0 +1,39 @@
+package com.anamariafelix.ms_ticket_manager.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocOpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("security", securityScheme()))
+                .info(new Info()
+                        .title("REST API - Microsservice Ticket")
+                        .description("API for Ticket management")
+                        .version("v1")
+                        .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0"))
+                        .contact(new Contact()
+                                .name("Ana Maria")
+                                .email("anafelix0909@gmail.com")));
+    }
+
+    private SecurityScheme securityScheme() {
+        return new SecurityScheme()
+                .description("Enter a valid bearer token to proceed")
+                .type(SecurityScheme.Type.HTTP)
+                .in(SecurityScheme.In.HEADER)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .name("security");
+    }
+}
+
